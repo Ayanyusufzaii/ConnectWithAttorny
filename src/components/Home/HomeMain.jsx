f49bebeb33b149d0263a3cf5f5fa66c9556c56c7
Ayan <ayan.yusufmail@gmail.com>
2025-07-08 12:03:59 +0530

    .

--- src/components/Home/HomeMain.jsx
+++ src/components/Home/HomeMain.jsx
@@ -10,7 +10,7 @@ import HomeFour from './HomeFour.jsx';
 import HomeFive from './HomeFive.jsx';
 import HomeSix from './HomeSix.jsx';
 
-const TURNSTILE_SITE_KEY = process.env.REACT_APP_TURNSTILE_SITE_KEY;
+const TURNSTILE_SITE_KEY = process.env.REACT_APP_TURNSTILE_SITE_KEY||'x4AAAAAABilXDcqr5nBkhD9';
 
 const HomeMain = () => {
   const [verified, setVerified] = useState(false);
